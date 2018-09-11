ee905cfc3f3e52dd8a6e39c533acc60feba7feb5
zackotterstrom <zack.otterstrom@elev.ga.ntig.se>
2018-09-11 15:24:12 +0200

    skapat login

--- lib/pluggy/controllers/user_controller.ex
+++ lib/pluggy/controllers/user_controller.ex
@@ -1,7 +1,11 @@
 defmodule Pluggy.UserController do
 
-	#import Pluggy.Template, only: [render: 2]
+	import Pluggy.Template, only: [render: 2]
 	import Plug.Conn, only: [send_resp: 3]
+
+	def index(conn) do
+		send_resp(conn, 200, render("fruits/index", []))
+	end
 
 	def login(conn, params) do
 		username = params["username"]
