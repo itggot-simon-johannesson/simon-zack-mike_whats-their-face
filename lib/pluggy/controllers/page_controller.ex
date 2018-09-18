0c5e0a5c3b4ea2bfc3cc670d548f3018ea68bf4a
Mike Johansson <mike.johansson@gmail.com>
2018-09-18 10:37:03 +0200

    Still trying

---
+++ lib/pluggy/controllers/page_controller.ex
@@ -0,0 +1,44 @@
+defmodule Pluggy.PageController do
+  
+    require IEx
+  
+    #alias Pluggy.User
+    import Pluggy.Template, only: [render: 2]
+    import Plug.Conn, only: [send_resp: 3]
+  
+  
+    def index(conn) do
+      send_resp(conn, 200, render("fruits/login", []))
+    end
+
+    def register(conn) do
+      send_resp(conn, 200, render("fruits/register", []))
+    end
+  
+#     def new(conn),          do: send_resp(conn, 200, render("fruits/new", []))
+#     def show(conn, id),     do: send_resp(conn, 200, render("fruits/show", fruit: Fruit.get(id)))
+#     def edit(conn, id),     do: send_resp(conn, 200, render("fruits/edit", fruit: Fruit.get(id)))
+    
+#     def create(conn, params) do
+#       Fruit.create(params)
+#       #move uploaded file from tmp-folder (might want to first check that a file was uploaded)
+#       File.rename(params["file"].path, "priv/static/uploads/#{params["file"].filename}")
+#       redirect(conn, "/fruits")
+#     end
+  
+#     def update(conn, id, params) do
+#       Fruit.update(id, params)
+#       redirect(conn, "/fruits")
+#     end
+  
+#     def destroy(conn, id) do
+#       Fruit.delete(id)
+#       redirect(conn, "/fruits")
+#     end
+  
+#     defp redirect(conn, url) do
+#       Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
+#     end
+  
+end
+  
--- lib/pluggy/router.ex
+++ lib/pluggy/router.ex
@@ -3,7 +3,7 @@ defmodule Pluggy.Router do
 
   alias Pluggy.FruitController
   alias Pluggy.UserController
-  alias Pluggy.GameController
+  alias Pluggy.PageController
 
   plug Plug.Static, at: "/", from: :pluggy
   plug(:put_secret_key_base)
@@ -27,12 +27,9 @@ defmodule Pluggy.Router do
   get "/fruits/new",       do: FruitController.new(conn)
   get "/fruits/:id",       do: FruitController.show(conn, id)
   get "/fruits/:id/edit",  do: FruitController.edit(conn, id)
-
-  get "/game_one", do: GameController.show_game_one(conn)
-  get "/game_two", do: GameController.show_game_two(conn)
-  get "/game_three", do: GameController.game_three(conn)
-
-  post "/game_two", do: GameController.show_game_two(conn)
+  # ---NEW---
+  get "/login",            do: PageController.index(conn)
+  get "/register",         do: PageController.register(conn)
   
   post "/fruits",          do: FruitController.create(conn, conn.body_params)
  
@@ -45,6 +42,8 @@ defmodule Pluggy.Router do
 
   post "/users/login",     do: UserController.login(conn, conn.body_params)
   post "/users/logout",    do: UserController.logout(conn)
+  #---NEW---
+  post "/users/register",  do: UserController.create(conn, conn.body_params)
 
   match _ do
     send_resp(conn, 404, "oops")
